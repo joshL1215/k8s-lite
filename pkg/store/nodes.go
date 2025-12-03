2a5f7686e8d31f64446f80605f1735643640e89e
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-12-02 17:27:38 -0800

    Restructure files for in mem store

---
+++ pkg/store/nodes.go
@@ -0,0 +1,1 @@
+// node methods for InMemoryStore here
--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,3 +1,28 @@
 // TODO: Build store w/ protobuf later
 // Currently in memory key pairing
 
+package store
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/joshL1215/k8s-lite/pkg/api"
+)
+
+type InMemoryStore struct {
+	mutex sync.RWMutex
+	pods  map[string]*api.Pod
+	// TODO: nodes later
+}
+
+func CreateInMemoryStore() *InMemoryStore {
+	return &InMemoryStore{
+		pods: make(map[string]*api.Pod),
+		// TODO: nodes later
+	}
+}
+
+func podKey(pod *api.Pod) string {
+	return fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
+}
