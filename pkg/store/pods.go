2c9149ea6546474b1a156cf30a761f965a37c6c8
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-12-02 17:35:21 -0800

    Add GetPod method and refactor podKey helper

--- pkg/store/pods.go
+++ pkg/store/pods.go
@@ -6,8 +6,8 @@ import (
 	"github.com/joshL1215/k8s-lite/pkg/api"
 )
 
-func podKey(pod *api.Pod) string {
-	return fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
+func podKey(namespace, name string) string {
+	return fmt.Sprintf("%s/%s", namespace, name)
 }
 
 // CreatePod
@@ -15,7 +15,7 @@ func (s *InMemoryStore) CreatePod(pod *api.Pod) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	key := podKey(pod)
+	key := podKey(pod.Namespace, pod.Name)
 	if _, exists := s.pods[key]; exists {
 		return fmt.Errorf("Pod %s already exists in namespace %s", pod.Name, pod.Namespace)
 	}
@@ -24,6 +24,17 @@ func (s *InMemoryStore) CreatePod(pod *api.Pod) error {
 }
 
 // GetPod
+func (s *InMemoryStore) GetPod(namespace, name string) (*api.Pod, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	key := podKey(namespace, name)
+	pod, exists := s.pods[key]
+	if !exists {
+		return nil, fmt.Errorf("No pod with name %s exists in namespace %s", name, namespace)
+	}
+	return pod, nil
+}
 
 // UpdatePod
 
