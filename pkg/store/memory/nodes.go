0a64c9d127fd5334aa63f04e4d385df79eb299cc
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-12-04 14:13:38 -0800

    add node store functions

--- pkg/store/memory/nodes.go
+++ pkg/store/memory/nodes.go
@@ -1,1 +1,62 @@
-// node methods for InMemoryStore here
+package memory
+
+import (
+	"fmt"
+
+	"github.com/joshL1215/k8s-lite/pkg/api"
+)
+
+func (s *InMemoryStore) CreateNode(node *api.Node) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if _, exists := s.nodes[node.Name]; exists {
+		return fmt.Errorf("a node named %s already exists", node.Name)
+	}
+	s.nodes[node.Name] = node
+	return nil
+}
+
+func (s *InMemoryStore) GetNode(name string) (*api.Node, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	node, exists := s.nodes[name]
+	if !exists {
+		return nil, fmt.Errorf("no node named %s", name)
+	}
+	return node, nil
+}
+
+func (s *InMemoryStore) UpdateNode(node *api.Node) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if _, exists := s.nodes[node.Name]; !exists {
+		return fmt.Errorf("no node named %s to update", node.Name)
+	}
+	s.nodes[node.Name] = node
+	return nil
+}
+
+func (s *InMemoryStore) DeleteNode(name string) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if _, exists := s.nodes[name]; !exists {
+		return fmt.Errorf("no node named %s to delete", name)
+	}
+	delete(s.nodes, name)
+	return nil
+}
+
+func (s *InMemoryStore) ListNodes() ([]*api.Node, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	nodeList := make([]*api.Node, 0)
+	for _, node := range s.nodes {
+		nodeList = append(nodeList, node)
+	}
+	return nodeList, nil
+}
