b7ba18cf5e244ec88e66fced65ef8e445a2f60b6
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-12-04 23:30:24 -0800

    Allow api server to start

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joshL1215/k8s-lite/pkg/store"
+	"github.com/joshL1215/k8s-lite/pkg/store/memory"
+)
+
+const DefaultPort = "8080"
+
+type APIServer struct {
+	router *gin.Engine
+	store  store.StoreInterface // having an interface here makes it store-implementation-agnostic
+}
+
+func (s *APIServer) Serve(port string) {
+	log.Println("Serving API server...")
+	if err := s.router.Run(port); err != nil {
+		log.Printf("Could not serve API server: %v", err)
+	}
+}
+
+// func (s *APIServer) registerRoutes() {
+// 	podsGroup := s.router.Group("/api/v1/namespace/:namespace/pods") // version APIs for backwards compatability
+// 	{
+
+// 	}
+// }
+
+func CreateAPIServer(s store.StoreInterface) *APIServer {
+	apiServer := &APIServer{
+		router: gin.Default(),
+		store:  s,
+	}
+	// apiServer.registerRoutes()
+	return apiServer
+}
+
+func main() {
+	dataStore := memory.CreateInMemoryStore()
+	apiServer := CreateAPIServer(dataStore)
+	apiServer.Serve(":" + DefaultPort)
+}
